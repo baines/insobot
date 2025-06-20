d8135c04abf396f31ad4e5705dd2ce041439e600
Alex Baines <alex@abaines.me.uk>
2025-06-20 18:41:12 +0100

    trivia module

---
+++ src/mod_trivia.c
@@ -0,0 +1,196 @@
+#include "module.h"
+#include "inso_utils.h"
+#include "inso_json.h"
+#include "stb_sb.h"
+#include <argz.h>
+#include <stdbool.h>
+#include <curl/curl.h>
+#include <ctype.h>
+
+static bool trivia_init (const IRCCoreCtx*);
+static void trivia_msg  (const char* chan, const char* name, const char* msg);
+static void trivia_cmd  (const char* chan, const char* name, const char* arg, int cmd);
+static void trivia_tick (time_t now);
+
+enum { TRIVIA_CMD };
+
+const IRCModuleCtx irc_mod_ctx = {
+	.name     = "trivia",
+	.desc     = "Anime trivia game",
+	.on_init  = &trivia_init,
+	.on_cmd   = &trivia_cmd,
+	.on_tick  = &trivia_tick,
+	.on_msg   = &trivia_msg,
+	.commands = DEFINE_CMDS (
+		[TRIVIA_CMD] = "!trivia"
+	),
+};
+
+struct TriviaState {
+	char* channel;
+	char* question;
+	char* answer_argz;
+	size_t answer_sz;
+
+	time_t hint;
+	time_t end;
+};
+
+struct TriviaScore {
+	char* username;
+	int score;
+};
+
+static const IRCCoreCtx* ctx;
+static sb(struct TriviaState) state;
+//static sb(struct TriviaScore) scores;
+
+#include "trivia/anilist.c"
+#include "trivia/levenshtein.c"
+
+static bool trivia_init(const IRCCoreCtx* _ctx){
+	ctx = _ctx;
+	return true;
+}
+
+static void trivia_free(struct TriviaState* ts) {
+	free(ts->question);
+	ts->question = NULL;
+
+	free(ts->answer_argz);
+	ts->answer_argz = NULL;
+	ts->answer_sz = 0;
+	ts->end = 0;
+}
+
+static void trivia_hint(struct TriviaState* ts) {
+	assert(ts->channel);
+	assert(ts->answer_argz);
+
+	sb(char) hint = NULL;
+
+	char* answer_copy = strdupa(ts->answer_argz);
+	char* state = NULL;
+
+	for(char* word = strtok_r(answer_copy, " ", &state); word; word = strtok_r(NULL, " ", &state)) {
+		int wlen = strlen(word);
+		int viz_letter_count = INSO_MAX(1, wlen / 4);
+
+		for(int i = 0; i < wlen; ++i) {
+			bool keep = (rand() % (wlen - i)) < viz_letter_count;
+			if(keep) {
+				sb_push(hint, word[i]);
+				viz_letter_count--;
+			} else {
+				sb_push(hint, '_');
+			}
+		}
+
+		sb_push(hint, ' ');
+	}
+
+	sb_push(hint, '\0');
+
+	ctx->send_msg(ts->channel, "[Trivia] Hint: %s", hint);
+	sb_free(hint);
+}
+
+static struct TriviaState* trivia_get(const char* chan) {
+	sb_each(s, state) {
+		if(strcmp(s->channel, chan) == 0) {
+			return s;
+		}
+	}
+
+	struct TriviaState ts = {
+		.channel = strdup(chan)
+	};
+	sb_push(state, ts);
+
+	return &sb_last(state);
+}
+
+static void trivia_tick(time_t now) {
+	sb_each(ts, state) {
+		if(ts->end == 0) {
+			continue;
+		}
+
+		if(now > ts->end) {
+			ctx->send_msg(ts->channel, "[Trivia] Time's up - the answer was: %s\n", ts->answer_argz);
+			trivia_free(ts);
+		} else if(ts->hint && now > ts->hint) {
+			trivia_hint(ts);
+			ts->hint = 0;
+		}
+	}
+}
+
+static float str_similarity(const char* _a, const char* _b) {
+	char* a = strdupa(_a);
+	int len = 0;
+
+	for(char* p = a; *p; ++p) {
+		*p = tolower(*p);
+		++len;
+	}
+
+	char* b = strdupa(_b);
+	for(char* p = b; *p; ++p) {
+		*p = tolower(*p);
+	}
+
+	int n = levenshtein_n(a, len, b, strlen(b));
+	return (float)(len - n) / (float)len;
+}
+
+static void trivia_msg(const char* chan, const char* name, const char* msg) {
+	struct TriviaState* ts = trivia_get(chan);
+	if(ts->end == 0) {
+		return;
+	}
+
+	char* answer = NULL;
+	while((answer = argz_next(ts->answer_argz, ts->answer_sz, answer))) {
+		float f = str_similarity(msg, answer);
+		if (f > 0.85) {
+			if(f != 1.0f) {
+				ctx->send_msg(ts->channel, "[Trivia] Correct! The answer was: %s (%.2f%%)", answer, f);
+			} else {
+				ctx->send_msg(ts->channel, "[Trivia] Correct! The answer was: %s", answer);
+			}
+			trivia_free(ts);
+
+			// TODO: score
+
+			break;
+		}
+	}
+}
+
+static void trivia_start(const char* chan) {
+	struct TriviaState* ts = trivia_get(chan);
+
+	// already in progress
+	if(ts->end > 0) {
+		return;
+	}
+
+	if(trivia_start_anilist(chan, ts)) {
+		ctx->send_msg(chan, "[Trivia] %s", ts->question);
+	} else {
+		ctx->send_msg(chan, "[Trivia] error starting trivia :(");
+	}
+}
+
+static void trivia_cmd(const char* chan, const char* name, const char* arg, int cmd){
+	switch(cmd){
+		case TRIVIA_CMD: {
+			if(*arg++) {
+				//if(strcmp(arg, "leaderboard")
+			} else {
+				trivia_start(chan);
+			}
+		} break;
+	}
+}
---
+++ src/trivia/anilist.c
@@ -0,0 +1,177 @@
+
+static const char* graphql =
+"query($page: Int) {"
+" Page(perPage: 1, page: $page) {"
+"  media(type: ANIME, popularity_greater: 1000, sort: [ID]) {"
+"   id"
+"   title {"
+"    english,"
+"    romaji"
+"   },"
+"   startDate {"
+"    year"
+"   }"
+"   source,"
+"   studios(isMain: true) {"
+"    nodes {"
+"     name"
+"    }"
+"   },"
+"   characters(role: MAIN) {"
+"    nodes {"
+"     name {"
+"      full"
+"     }"
+"    }"
+"   }"
+"  }"
+" }"
+"}";
+
+static int max_page = 10105;
+
+enum quiz_types {
+	Q_YEAR    = (1 << 0),
+	Q_STUDIOS = (1 << 1),
+	Q_CHARS   = (1 << 2),
+
+	Q_COUNT   = 3
+};
+
+static bool trivia_start_anilist(const char* chan, struct TriviaState* ts) {
+	bool result = false;
+
+	char* data = NULL;
+	CURL* curl = inso_curl_init("https://graphql.anilist.co", &data);
+
+	int page = rand() % max_page;
+
+	char* json;
+	asprintf_check(&json, "{ \"query\": \"%s\", \"variables\": { \"page\": %d } }", graphql, page);
+
+	struct curl_slist* headers = curl_slist_append(NULL, "Content-Type: application/json");
+
+	curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json);
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	//curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+
+	long rsp = inso_curl_perform(curl, &data);
+
+	curl_slist_free_all(headers);
+
+	if (rsp != 200) {
+		printf("anilist bad rsp %ld %s\n", rsp, data);
+		goto end;
+	}
+
+	struct uj_node* root = uj_parse(data, strlen(data), NULL);
+	struct uj_node* media_array = UJ_GET(root, ("data", "Page", "media"), UJ_ARR);
+
+	if(!media_array) {
+		printf("no media array\n");
+		uj_node_free(root, 1);
+		goto end;
+	}
+
+	struct uj_node* media = media_array->arr;
+	struct uj_node* title_en = UJ_GET(media, ("title", "english"), UJ_STR);
+	struct uj_node* title_rj = UJ_GET(media, ("title", "romaji"), UJ_STR);
+
+	char* title;
+	if(title_en) {
+		title = title_en->str;
+	} else if(title_rj) {
+		title = title_rj->str;
+	} else {
+		uj_node_free(root, 1);
+		goto end;
+	}
+
+	printf("page %d title = [%s]\n", page, title);
+
+	int quiz_mask = 0;
+
+	struct uj_node* year = UJ_GET(media, ("startDate", "year"), UJ_INT);
+	if(year) {
+		quiz_mask |= Q_YEAR;
+	}
+
+	struct uj_node* studios_array = UJ_GET(media, ("studios", "nodes"), UJ_ARR);
+	sb(char*) studios = NULL;
+
+	for(size_t i = 0; i < studios_array->len; ++i) {
+		struct uj_node* s = UJ_GET(studios_array->arr + i, ("name"), UJ_STR);
+		if(s) {
+			sb_push(studios, s->str);
+		}
+	}
+
+	if(sb_count(studios) > 0) {
+		quiz_mask |= Q_STUDIOS;
+	}
+
+	struct uj_node* chars_array = UJ_GET(media, ("characters", "nodes"), UJ_ARR);
+	sb(char*) chars = NULL;
+
+	for(size_t i = 0; i < chars_array->len; ++i) {
+		struct uj_node* name = UJ_GET(chars_array->arr + i, ("name", "full"), UJ_STR);
+		if(name) {
+			sb_push(chars, name->str);
+		}
+	}
+
+	if(sb_count(chars) > 0) {
+		quiz_mask |= Q_CHARS;
+	}
+
+	if(quiz_mask == 0) {
+		printf("no quiz options...\n");
+		goto end;
+	}
+
+	int qmax = rand() % __builtin_popcount(quiz_mask);
+	int quiz_type;
+
+	for(int i = 0; i < Q_COUNT; ++i) {
+		int type = (1 << i);
+		if((quiz_mask & type) && qmax == 0) {
+			quiz_type = type;
+			break;
+		}
+
+		if(qmax > 0) {
+			--qmax;
+		}
+	}
+
+	if(quiz_type == Q_YEAR) {
+		asprintf_check(&ts->question, "In what year was \"%s\" released?", title);
+		char year_str[32];
+		snprintf(year_str, sizeof(year_str), "%d", (int)year->num);
+		argz_add(&ts->answer_argz, &ts->answer_sz, year_str);
+	} else if(quiz_type == Q_STUDIOS) {
+		asprintf_check(&ts->question, "Which studio produced \"%s\"?", title);
+		sb_each(s, studios) {
+			argz_add(&ts->answer_argz, &ts->answer_sz, *s);
+		}
+	} else if(quiz_type == Q_CHARS) {
+		asprintf_check(&ts->question, "Name a main character from \"%s\"", title);
+		sb_each(c, chars) {
+			argz_add(&ts->answer_argz, &ts->answer_sz, *c);
+		}
+	} else {
+		printf("ERM\n");
+		goto end;
+	}
+
+	ts->hint = time(0) + 30;
+	ts->end  = time(0) + 60;
+	result   = true;
+
+end:
+	curl_easy_cleanup(curl);
+	free(json);
+	sb_free(data);
+	return result;
+}
